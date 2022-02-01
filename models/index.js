05e5e23b88783536a9a1c5cfab1507440b9e9c1f
scottrohrig <scott.rohrig@gmail.com>
2022-02-01 11:24:08 -0800

    add Post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // This file will become more important as we create more models, but for now it'll just be for collecting and exporting the User model data.
 
 const User = require('./User');
+const Post = require( './Post' );
 
-module.exports = User;
+module.exports = { User, Post };
