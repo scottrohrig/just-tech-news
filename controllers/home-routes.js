d73d8b5655588a1695298baf5dce4d6318d97afa
scottrohrig <scott.rohrig@gmail.com>
2022-02-04 17:09:39 -0800

    add homepage template

---
+++ controllers/home-routes.js
@@ -0,0 +1,51 @@
+const router = require( 'express' ).Router();
+const sequelize = require( '../config/connection' );
+const { User, Post, Comment } = require( '../models' );
+
+// router.get('/', (req, res)=>{
+//   res.render('homepage', { // I'm a sample Post object!
+//     id: 1,
+//     post_url: 'https://handlebarsjs.com/guide/',
+//     title: 'Handlebars Docs',
+//     created_at: new Date(),
+//     vote_count: 10,
+//     comments: [{}, {}],
+//     user: {
+//       username: 'test_user'
+//     }
+//   });
+// })
+
+router.get( '/', ( req, res ) => {
+  Post.findAll( {
+    attributes: [
+      'id', 'post_url', 'title', 'created_at',
+      [ sequelize.literal( `(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)` ), 'vote_count' ]
+    ],
+    include: [
+      {
+        model: User,
+        attributes: [ 'username' ]
+      },
+      {
+        model: Comment,
+        attributes: [ 'id', 'comment_text', 'post_id', 'user_id', 'created_at' ],
+        include: {
+          model: User,
+          attributes: [ 'username' ]
+        }
+      }
+    ]
+  } )
+    .then( dbPosts => {
+
+      const posts = dbPosts.map( post => post.get( { plain: true } ) );
+
+      res.render(
+        'homepage', { posts } );
+
+    } )
+    .catch( err => res.status( 500 ).json( err ) );
+} );
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-// import express, routes, sequelize 'connection'
-const express = require( 'express' );
-const routes = require( './routes' );
-const sequelize = require( './config/connection' );
+// libraries
+const path = require( "path" );
+const express = require( "express" );
+const exphbs = require( "express-handlebars" );
 
-// set port
+// app modules
+const routes = require( "./controllers" );
+const sequelize = require( "./config/connection" );
+
+// setup app properties
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// create a session
+// use the session
+
+// setup handlebars configuration
+const hbs = exphbs.create( {} );
+
+// configure the template engine
+app.engine( "handlebars", hbs.engine );
+app.set( "view engine", "handlebars" );
+
 // connect middleware
-app.use( express.urlencoded( { extended: true } ) );
 app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.static( path.join( __dirname, "public" ) ) );
 
 // turn on routes
-app.use( '/', routes );
+app.use( "/", routes );
 
 // turn on connection to db && server
 sequelize.sync( { force: false } ).then( () => {
-    app.listen( PORT, () => console.log( 'Listening' ) );
+    app.listen( PORT, () => console.log( `Listening on port ${ PORT }` ) );
 } );
